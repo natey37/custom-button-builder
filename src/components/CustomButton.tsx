05f42449f43b78c898e422a95b70878e223dcce2
Nathan Brickett <nathanbrickett@gmail.com>
2023-01-26 21:39:26 -0800

    Create button component to display users button

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -1,18 +1,62 @@
 import React from 'react';
+import { TextAlign } from '../enums';
 
 interface Props {
+    content: string;
     backgroundColor: string;
-    textColor: string;
+    borderWidth: string;
+    borderStyle: string;
+    borderColor: string;
     borderRadius: string;
+    color: string;
     fontSize: string;
+    padding: string;
+    textAlign: TextAlign;
+    textDecoration: string;
+    width: string;
+    height: string;
+    boxShadow: string;
+    transition: string;
 }
 
 const CustomButton: React.FC<Props> = (props) => {
-    const { backgroundColor, textColor, borderRadius, fontSize } = props;
+    const { 
+        content,
+        backgroundColor, 
+        borderWidth, 
+        borderStyle, 
+        borderColor,
+        borderRadius, 
+        color, 
+        fontSize, 
+        padding, 
+        textAlign, 
+        textDecoration, 
+        width, 
+        height, 
+        boxShadow, 
+        transition 
+    } = props;
 
     return (
-        <button style={{ backgroundColor: backgroundColor, color: textColor, borderRadius: borderRadius, fontSize: fontSize}}>
-            Customizable Button
+        <button 
+            style={{
+                backgroundColor: backgroundColor, 
+                borderWidth: borderWidth,
+                borderStyle: borderStyle,
+                borderColor: borderColor,
+                color: color, 
+                borderRadius: borderRadius, 
+                fontSize: fontSize,
+                padding: padding,
+                textAlign: textAlign,
+                textDecoration: textDecoration,
+                width: width,
+                height: height,
+                boxShadow: boxShadow,
+                transition: transition
+            }}>
+            {content}
         </button>
     );
 };
