0c09eb44477d70c2c84aae0a395271022dca3b90
Nathan Brickett <nathanbrickett@gmail.com>
2023-01-27 19:09:22 -0800

    Updates

--- src/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CustomButtonBuilder from './CustomButtonBuilder';
-
-ReactDOM.render(<CustomButtonBuilder />, document.getElementById('root'));
